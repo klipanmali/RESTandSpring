e0183a2702b4b20890c4a01563cd11faecf525dc
T H <T H>
2019-06-30 19:32:10 +0200

    Hello World - spring boot

---
+++ src/main/java/com/example/spring5andrest/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.spring5andrest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello world";
+	}
+
+}
